f51607e4e8138708273e9cedeb3db2669d9e1a2b
farishuda <farishudaku@gmail.com>
2026-02-19 22:48:31 +0700

    Initial Setup

---
+++ src/test/java/com/b9/json/jsonplatform/JsonPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b9.json.jsonplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
