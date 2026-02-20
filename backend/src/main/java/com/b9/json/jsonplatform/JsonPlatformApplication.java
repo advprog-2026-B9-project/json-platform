e9e818e285d9c92e8c6232d9be910fd99b75c7c7
farishuda <farishudaku@gmail.com>
2026-02-20 17:59:15 +0700

    chore: setup mono repo

---
+++ backend/src/main/java/com/b9/json/jsonplatform/JsonPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.b9.json.jsonplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonPlatformApplication.class, args);
+    }
+
+}
